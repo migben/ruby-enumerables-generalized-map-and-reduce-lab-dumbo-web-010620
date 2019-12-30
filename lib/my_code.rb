51756f29fecee71e2bf3a572f0b500b43b173b29
Miguel Ben <itsmigben@gmail.com>
2019-12-30 10:00:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -11,14 +11,10 @@ def map(arr)
   
 end
 
-def reduce(arr, start = 0)
-  acc = start
-  
-  start != false ? acc = start : acc = arr[0]
-  
-  arr.each {|ele|
-    acc = yield(acc, ele)
-  }
+def reduce(arr, start = nil)
+  if start
+    
+    
   
   return acc
   
