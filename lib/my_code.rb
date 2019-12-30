e6cf3eb96d0aa5a03eb178d55a8a4ed92708f87d
Miguel Ben <itsmigben@gmail.com>
2019-12-30 10:07:45 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,33 @@
 # Your Code Here
+def map(arr)
+  
+  result = []
+  
+  arr.each { |ele|
+    result.push(yield(ele))
+  }
+  
+  return result
+  
+end
+
+def reduce(arr, start = nil)
+  if start
+    
+    acc = start
+    idx = 0
+  else
+    acc = arr[0]
+    idx = 1
+  end
+  
+  while idx < arr.length
+    
+    acc =  yield(acc, arr[idx])
+    idx += 1
+    
+  end
+  
+  return acc
+  
+end
